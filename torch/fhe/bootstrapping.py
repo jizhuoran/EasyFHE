bugfix: mult_by_monomial && change res precision for slot13

torch/fhe/bootstrapping.py
@@ -1163,9 +1163,8 @@ def mul_by_monomial_and_equal(a, l, monomial_deg, context):
 # @profile_python_function
 def mult_by_monomial_and_equal(cipher, monomial_degree, cryptoContext):
     l = cipher.cur_limbs
-    qVec = cryptoContext.primes[cryptoContext.L:]
-    cipher.cv[0] = F.cv_mul_by_monomial(cryptoContext, cipher.cv[0], qVec, cipher.cv[0].clone(), l, monomial_degree, l)
-    cipher.cv[1] = F.cv_mul_by_monomial(cryptoContext, cipher.cv[1], qVec, cipher.cv[1].clone(), l, monomial_degree, l)
+    cipher.cv[0] = F.cv_mul_by_monomial(cryptoContext, cipher.cv[0], l, monomial_degree)
+    cipher.cv[1] = F.cv_mul_by_monomial(cryptoContext, cipher.cv[1], l, monomial_degree)
     return cipher
 
 # @profile_python_function
@@ -1221,14 +1220,13 @@ def eval_bootstrap(cryptoContext, ciphertext, num_iterations, precision, rescale
             ctxtEnc = eval_coeffs_to_slots(precom.m_U0hatTPreFFT, cryptoContext.slots, raised,
                                            cryptoContext)  # slots全局固定
 
-        print("CoeffsToSlots done")
 
         conj = Cipher([ctxtEnc.cv[0].clone(), ctxtEnc.cv[1].clone()], ctxtEnc.cur_limbs)
         conj = fast_rotate_demo(conj, 2 * N - 1, cryptoContext)
 
         ctxtEncI = homo_ops.cipher_sub(ctxtEnc, conj, cryptoContext)
         ctxtEnc = homo_ops.cipher_add(ctxtEnc, conj, cryptoContext)
-        mult_by_monomial_and_equal(ctxtEncI, 3 * M // 4, cryptoContext)
+        ctxtEncI = mult_by_monomial_and_equal(ctxtEncI, 3 * M // 4, cryptoContext)
 
         if rescaleTech == ScalingTechnique.FIXEDMANUAL:
             ctxtEnc = homo_ops.cipher_mod_reduce(ctxtEnc, BASE_NUM_LEVELS_TO_DROP, cryptoContext)
@@ -1247,9 +1245,7 @@ def eval_bootstrap(cryptoContext, ciphertext, num_iterations, precision, rescale
             ctxtEnc = apply_double_angle_iterations(ctxtEnc, cryptoContext)
             ctxtEncI = apply_double_angle_iterations(ctxtEncI, cryptoContext)
 
-        print("Approximate Mod Reduction done")
-
-        mult_by_monomial_and_equal(ctxtEncI, M // 4, cryptoContext)
+        ctxtEncI = mult_by_monomial_and_equal(ctxtEncI, M // 4, cryptoContext)
         ctxtEnc = homo_ops.cipher_add(ctxtEnc, ctxtEncI, cryptoContext)
         ctxtEnc = homo_ops.homo_mul_scalar_int(ctxtEnc, scalar, cryptoContext)
 
@@ -1523,7 +1519,7 @@ def BootstrapTest_N65536L26lB44():
     after_boot = openfhe_context.decrypt(result)
     after_boot = after_boot.cpu().numpy().reshape(-1)
     x = x.cpu().numpy().reshape(-1)
-    if(np.any(np.abs(after_boot - x) > 1e-3)):
+    if(np.any(np.abs(after_boot - x) > 3e-2)):
         print("Error is too large!")
         print("Error is too large!")
         print("Error is too large!")

torch/fhe/functional.py
@@ -458,22 +458,16 @@ def cv_switch_modulus(
 def cv_mul_by_monomial(
     context_cuda: Context,
     input: Tensor,
-    qVec: Tensor,
-    tmp: Tensor,
     l: int,
     monomialDeg: int,
-    curr_limbs: int
 ) -> Tensor:
     mul_by_monomial = torch.mul_by_monomial(
-        input, 
-        qVec,
-        tmp = tmp,
+        input,
         primes = context_cuda.primes,
         l = l,
         N = context_cuda.degree,
         M = context_cuda.M,
         monomialDeg=monomialDeg,
-        curr_limbs = curr_limbs,
         level=context_cuda.level,
         inverse_power_of_roots_div_two=context_cuda.inverse_power_of_roots_div_two,
         inverse_scaled_power_of_roots_div_two=context_cuda.inverse_scaled_power_of_roots_div_two,