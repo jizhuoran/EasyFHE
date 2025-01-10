a77fc92ebfe0dec4942b378b9b8a302b66f1a2b7
wenzhe.wang <wongwwz@foxmail.com>
2025-01-10 11:24:58 +0800

    Add test writing out file

--- torch/fhe/example/run_all_test.py
+++ torch/fhe/example/run_all_test.py
@@ -1,14 +1,22 @@
 import pickle, sys, os
 import numpy as np
+import csv
+import time
+
 sys.path.append("/".join(os.getcwd().split("/")[:-3]))
 sys.path.append("/".join(os.getcwd().split("/")[:-2]))
 import torch
 import torch.fhe.bootstrapping as BS
 import torch.fhe.utils as utils
 
-#find all context in the directory
+# find all context in the directory
 all_correct = True
 path = "data/"
+f = open('./test_res.csv', 'w', encoding='utf-8', newline="")
+csv_write = csv.writer(f)
+csv_write.writerow(
+    ['logN', 'logSlots', 'maxLevelsRemaining', 'levelBudget', 'dnum', 'dcrtBits', 'firstMod', 'approxModDepth',
+     'rescaleTech', 'res', 'exec_time'])
 for context_file in os.listdir(path):
     if context_file.endswith(".pkl") and "GPU-FHE-CONTEXT" in context_file:
         context_file = context_file.replace("_UNIFORM_TERNARY_", "_")
@@ -23,7 +31,7 @@ for context_file in os.listdir(path):
         dcrtBits = int(dcrtBits)
         firstMod = int(firstMod)
         approxModDepth = int(approxModDepth)
-        
+
         cryptoContext, openfhe_context = utils.try_load_context(
             int(logN),
             int(logSlots),
@@ -39,20 +47,54 @@ for context_file in os.listdir(path):
 
         # Test the correctness of the bootstrapping
         values = [0.111111, 0.222222, 0.333333, 0.444444, 0.555555, 0.666666, 0.777777, 0.888888]
-        x = np.array([values[i % len(values)] for i in range((1<<logSlots))])
+        x = np.array([values[i % len(values)] for i in range((1 << logSlots))])
         x = torch.tensor(x, device="cuda")
         cipher, cipher_openfhe = openfhe_context.encrypt(x, 1, openfhe_context.depth - 1)
-
-        result = BS.eval_bootstrap(cipher, L0=cryptoContext.L, slots=(1<<logSlots), cryptoContext=cryptoContext)
-        openfhe_result = openfhe_context.cc.EvalBootstrap(cipher_openfhe)
-        data = np.array(openfhe_result.GetVectorOfData(), dtype=np.uint64)
-        if np.equal(np.concatenate([result.cv[0].cpu().numpy(), result.cv[1].cpu().numpy()]).reshape(-1), data.reshape(-1)).all():
-            print("Test passed!")
-        else:
-            print("Test failed!")
-            print("result", result.cv[0].cpu().numpy()[0][:10])
-            print("data", data.reshape(-1)[:10])
+        try:
+            start = time.time()
+            result = BS.eval_bootstrap(cipher, L0=cryptoContext.L, slots=(1 << logSlots), cryptoContext=cryptoContext)
+            end = time.time()
+            openfhe_result = openfhe_context.cc.EvalBootstrap(cipher_openfhe)
+            # data = np.array(openfhe_result.GetVectorOfData(), dtype=np.uint64)
+            is_equal = utils.compare_bs_ct_with_openfhe(result, openfhe_result)
+            if is_equal:
+                csv_write.writerow([logN,
+                                    logSlots,
+                                    maxLevelsRemaining,
+                                    [levelBudget0, levelBudget1],
+                                    dnum,
+                                    dcrtBits,
+                                    firstMod,
+                                    approxModDepth,
+                                    rescaleTech, "pass", end - start])
+                print("Test passed!")
+            else:
+                csv_write.writerow([logN,
+                                    logSlots,
+                                    maxLevelsRemaining,
+                                    [levelBudget0, levelBudget1],
+                                    dnum,
+                                    dcrtBits,
+                                    firstMod,
+                                    approxModDepth,
+                                    rescaleTech, "fail", end - start])
+                print("Test failed!")
+                all_correct = False
+        except:
+            csv_write.writerow([logN,
+                                logSlots,
+                                maxLevelsRemaining,
+                                [levelBudget0, levelBudget1],
+                                dnum,
+                                dcrtBits,
+                                firstMod,
+                                approxModDepth,
+                                rescaleTech, "exception", 0])
+            print("Something wrong !!!")
             all_correct = False
+            f.close()
+            break
 
+f.close()
 if all_correct:
     print("All test cases passed!")
