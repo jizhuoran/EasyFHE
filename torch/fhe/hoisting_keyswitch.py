bafe0c817c5bf8389a276e12b7dcf8f4378896f7
yhh <yhh21@mail.sdu.edu.cn>
2025-01-16 18:20:05 +0800

    add `eval_fast_rotation`

--- torch/fhe/hoisting_keyswitch.py
+++ torch/fhe/hoisting_keyswitch.py
@@ -71,6 +71,24 @@ def fused_rotation_add_ext(digits, cipher, index, cryptoContext):
     return digits.cipher_like([cv0, cv1], is_ext=True)
 
 
+def eval_fast_rotation(ciphertext, index, digits, cryptoContext):
+    if index == 0:
+        return ciphertext.clone()
 
+    cur_limbs = ciphertext.cur_limbs
 
+    # EvalFastKeySwitchCore = InnerProduct + ModDown
+    auto_index = cryptoContext.find_auto_index(index)
+    swk = cryptoContext.left_rot_key_map[str(auto_index)]
+    sum_mult = F.cv_innerproduct(digits.cv[0].reshape(-1), curr_limbs=digits.cur_limbs, context=cryptoContext,
+                                 swk_bx=swk[0], swk_ax=swk[1])
+    sumMult = Cipher(sum_mult, ciphertext.cur_limbs, ciphertext.scaling_factor, ciphertext.noise_deg, ciphertext.slots, is_ext=True)
+    result = moddown_from_ext(sumMult, cryptoContext)
+    # post add after ks
+    result.cv[0] = F.cv_add(ciphertext.cv[0], result.cv[0], cryptoContext.moduliQ_cuda, cur_limbs)
 
+    # Apply the AutomorphismTransform to ax and bx
+    result.cv[0] = F.cv_automorphism_transform(result.cv[0], cur_limbs, auto_index, cryptoContext)
+    result.cv[1] = F.cv_automorphism_transform(result.cv[1], cur_limbs, auto_index, cryptoContext)
+
+    return result
