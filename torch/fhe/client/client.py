e88e97f1fbc60b9540d7aff0e35e6311f7b598e6
yhh <yhh21@mail.sdu.edu.cn>
2025-01-19 21:16:56 +0800

    bug fix: `encrypt` add support for `(np.ndarray, torch.Tensor)` and `list`

--- torch/fhe/client/client.py
+++ torch/fhe/client/client.py
@@ -22,7 +22,7 @@ class OpenFHEContext:
         openfhe.DeserializeEvalMultKeyString(content_map["mul_key"], openfhe.BINARY)
         openfhe.DeserializeEvalAutomorphismKeyString(content_map["rot_key"], openfhe.BINARY)
 
-    def encode(self, x, level=None, scale_deg=None, slots=None):
+    def encode(self, x, level=None, scale_deg=None, slots=None): # todo: align the input order wtih the encrypt function
         if not ((scale_deg is None and level is None and slots is None) or
                 (scale_deg is not None and level is not None and slots is not None)):
             # 输出警告
@@ -41,16 +41,19 @@ class OpenFHEContext:
                 ptx = self.cc.MakeCKKSPackedPlaintext(x, scale_deg, level, None, slots)
             ptx.Encode()
             data = ptx.GetVectorOfData()
-            cv = [torch.tensor(data, device="cuda", dtype=torch.uint64)]
+            cv = [torch.tensor(data, device="cuda", dtype=torch.uint64)] #fixme: shall we set device = "cuda" directly?
             return Plaintext(cv, None, ptx.GetSlots(), cv[0].shape[0], ptx.GetScalingFactor(), ptx.GetNoiseScaleDeg())
 
     def encrypt(self, x, scale_deg = 1, level = 0, slots= None):
         if slots is None:
             slots = len(x)
-        ptx = self.cc.MakeCKKSPackedPlaintext(x.tolist(), scale_deg, level, None, slots)
+        if isinstance(x, (np.ndarray, torch.Tensor)):
+            ptx = self.cc.MakeCKKSPackedPlaintext(x.tolist(), scale_deg, level, None, slots)
+        else:
+            ptx = self.cc.MakeCKKSPackedPlaintext(x, scale_deg, level, None, slots)
         cipher = self.cc.Encrypt(self.publicKey, ptx)
         data = cipher.GetVectorOfData()
-        cv = [torch.tensor(elem, device=x.device, dtype=torch.uint64) for elem in data]
+        cv = [torch.tensor(elem, device="cuda", dtype=torch.uint64) for elem in data] #fixme: shall we set device = "cuda" directly?
         return Cipher.Cipher(cv, cv[0].shape[0], cipher.GetScalingFactor(), cipher.GetNoiseScaleDeg(), cipher.GetSlots(), is_ext=False), cipher
     
     def decrypt(self, x):
