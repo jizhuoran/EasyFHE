3e6ddcc74d67da66606fa614d40a4f9ccc7ce1a5
jizhuoran <zrji@sdu.edu.cn>
2025-03-01 11:10:38 +0800

    fix run_test.py, change homoBS to evalBS

--- torch/fhe/example/run_test.py
+++ torch/fhe/example/run_test.py
@@ -5,11 +5,10 @@ sys.path.append("/".join(os.getcwd().split("/")[:-3]))
 sys.path.append("/".join(os.getcwd().split("/")[:-2]))
 import torch
 import torch.fhe.bootstrapping as BS
-import torch.fhe.bs_compilered as COMPILE
 import torch.fhe.utils as utils
 
+
 maxLevelsRemaining = 3
-appRotIndex_list = [-1, 2]
 logBsSlots_list = [8]
 logN = 14
 dnum = 3
@@ -17,28 +16,30 @@ dcrtBits = 52
 firstMod = 56
 levelBudget_list = [[4, 4]]
 rescaleTech = "FLEXIBLEAUTO"  # "FLEXIBLEAUTO" # "FIXEDMANUAL" # "FIXEDAUTO"
-save_dir = "data/"
+path = "data"
 mode = "debug"  # "debug" or "release"
-autoLoadAndSetConfig = True  # note: currently only support True
-
 secretKeyDist = "UNIFORM_TERNARY"  # "SPARSE_TERNARY"  "UNIFORM_TERNARY"
 
+
 cryptoContext, openfhe_context, openfhe_boot_contexts = utils.try_load_context(
-    maxLevelsRemaining,
-    appRotIndex_list,
+    int(maxLevelsRemaining),
+    [],
     logBsSlots_list,
-    logN,
-    dnum,
-    dcrtBits,
-    firstMod,
+    int(logN),
+    int(dnum),
+    int(dcrtBits),
+    int(firstMod),
     levelBudget_list,
     secretKeyDist,
     rescaleTech,
-    save_dir=save_dir,
+    save_dir=path,
     autoLoadAndSetConfig=True,
-    mode=mode,
+    mode="debug",
 )
 
+logBsSlots = logBsSlots_list[0]
+
+# Test the correctness of the bootstrapping
 values = [
     0.111111,
     0.222222,
@@ -49,12 +50,15 @@ values = [
     0.777777,
     0.888888,
 ]
-encode_slots = 1 << logBsSlots_list[0]
-x = np.array([values[i % len(values)] for i in range(encode_slots)])
+x = np.array([values[i % len(values)] for i in range((1 << logBsSlots))])
 x = torch.tensor(x, device="cuda")
 cipher, cipher_openfhe = openfhe_context.encrypt(
-    x, 1, openfhe_context.depth - 1, encode_slots
-)
+    x, 1, openfhe_context.depth - 1, (1 << logBsSlots)
+)  # specify the slots value explicitly
+
+cryptoContext.BsContext = cryptoContext.BsContext_map[str(logBsSlots)]
+cryptoContext.BsContext.to_cuda()
+utils.load_rotation_keys(logBsSlots, cryptoContext)
 
 # with torch.profiler.profile(
 #         activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
@@ -75,14 +79,25 @@ cipher, cipher_openfhe = openfhe_context.encrypt(
 # # Print the profiling summary in a table format
 # print(profiler_results.table(sort_by="self_cuda_time_total"))
 
+
 TEST_COMPILE = False
 
 if TEST_COMPILE:
-    result1 = BS.eval_bootstrap(cipher, L0=cryptoContext.L, logBsSlots=logBsSlots_list[0], cryptoContext=cryptoContext)
+    result1 = BS.eval_bootstrap(
+        cipher,
+        L0=cryptoContext.L,
+        logBsSlots=logBsSlots_list[0],
+        cryptoContext=cryptoContext,
+    )
     print("=======================")
     print("=======================")
     print("=======================")
-    result2 = COMPILE.eval_bootstrap(cipher, L0=cryptoContext.L, logBsSlots=logBsSlots_list[0], cryptoContext=cryptoContext)
+    result2 = COMPILE.eval_bootstrap(
+        cipher,
+        L0=cryptoContext.L,
+        logBsSlots=logBsSlots_list[0],
+        cryptoContext=cryptoContext,
+    )
 
     print("result1", result1.cv[0].cpu().numpy()[0][:10])
     print("result2", result2.cv[0].cpu().numpy()[0][:10])
@@ -98,17 +113,16 @@ if TEST_COMPILE:
 
 else:
 
-    _ = BS.eval_bootstrap(cipher, L0=cryptoContext.L, logBsSlots=logBsSlots_list[0], cryptoContext=cryptoContext)
+    result = BS.eval_bootstrap(
+        cipher, L0=cryptoContext.L, logBsSlots=logBsSlots, cryptoContext=cryptoContext
+    )
 
     start_time = time.time()
-    result = BS.homo_bootstrap(
-        cipher,
-        L0=cryptoContext.L,
-        logBsSlots=logBsSlots_list[0],
-        cryptoContext=cryptoContext,
+    result = BS.eval_bootstrap(
+        cipher, L0=cryptoContext.L, logBsSlots=logBsSlots, cryptoContext=cryptoContext
     )
-    print("Time for BS: ", time.time() - start_time)
-    openfhe_boot_context = openfhe_boot_contexts[str(logBsSlots_list[0])]
+    print("Time taken for bootstrapping:", time.time() - start_time)
+    openfhe_boot_context = openfhe_boot_contexts[str(logBsSlots)]
     openfhe_result = openfhe_boot_context.cc.EvalBootstrap(cipher_openfhe)
     data = np.array(openfhe_result.GetVectorOfData(), dtype=np.uint64)
     is_equal = utils.compare_bs_ct_with_openfhe(result, openfhe_result)
