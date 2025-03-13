4eec85d30e615ae64dd0d0c69a3d47d952ba2819
yhh <931323942@qq.com>
2025-03-13 21:04:37 +0800

    update homo_inner_product_hoisting

--- app/inner_product/innerproduct_example.py
+++ app/inner_product/innerproduct_example.py
@@ -1,10 +1,17 @@
 ##########################
 #### example for app #####
 ##########################
+import sys, os, time
+import numpy as np
+
+sys.path.append("/".join(os.getcwd().split("/")[:-2]))
+sys.path.append("/".join(os.getcwd().split("/")[:-3]))
+
 import torch.fhe as fhe
+import torch.fhe.homo_ops as homo_ops
 import numpy as np
 import torch
-import warnings,os
+import os
 import math
 
 
@@ -21,6 +28,48 @@ def homo_inner_product(cipher_A, cipher_B, cryptoContext):
     
     return cipher_product
 
+
+def homo_inner_product_hoisting(cipher_A, cipher_B, cryptoContext):
+    cipher_product = fhe.homo_mul(cipher_A, cipher_B, cryptoContext)
+    cipher_product = fhe.homo_rescale(cipher_product, 1, cryptoContext)
+    n = cipher_product.slots
+    assert (n & (n - 1)) == 0, "n must be a power of two"
+    log_n = int(math.log2(n))
+    shifts = [2 ** i for i in range(log_n)]
+
+
+    bxExt = fhe.key_switch_P_ext(fhe.extract_cv(cipher_product,0), cryptoContext)
+    ax = fhe.extract_cv(cipher_product,1)
+    for shift in shifts:
+        # ver1
+        # rotated = fhe.homo_rotate(cipher_product, shift, cryptoContext)
+
+        # ver2
+        # rotated_modup = fhe.modup_to_ext(fhe.extract_cv(cipher_product, 1), cryptoContext)
+        # rotated_innerp = fhe.mult_rot_key_and_sum_ext(rotated_modup, shift, cryptoContext)
+        # rotated = fhe.moddown_from_ext(rotated_innerp, cryptoContext)
+        # rotated.cv[0] = F.cv_add(rotated.cv[0], cipher_product.cv[0], cryptoContext.moduliQ, rotated.cur_limbs)
+        # rotated = homo_ops._cipher_automorphism(rotated, cryptoContext.norm_rot_index(shift), cryptoContext, printInfo=False)
+
+        # ver3
+        rotated_modup = fhe.modup_to_ext(ax, cryptoContext)
+        norm_index = cryptoContext.norm_rot_index(shift)
+        rotated_innerp = fhe.mult_rot_key_and_sum_ext(rotated_modup, norm_index, cryptoContext)
+
+        tmp_bxExt = fhe.homo_add(fhe.extract_cv(rotated_innerp,0), bxExt, cryptoContext)
+        tmp_ax    = fhe.moddown_from_ext(fhe.extract_cv(rotated_innerp,1), cryptoContext)
+
+        tmp_bxExt = homo_ops._cipher_automorphism(tmp_bxExt, norm_index, cryptoContext)
+        tmp_ax  = homo_ops._cipher_automorphism(tmp_ax, norm_index, cryptoContext)
+
+        bxExt = fhe.homo_add(tmp_bxExt, bxExt, cryptoContext)
+        ax    = fhe.homo_add(tmp_ax, ax, cryptoContext)
+
+    bx = fhe.moddown_from_ext(bxExt, cryptoContext)
+    cipher_product = cipher_product.cipher_like([bx.cv[0], ax.cv[0]])
+
+    return cipher_product
+
 def plain_inner_product(x, x1):
     return torch.dot(x, x1)
 
@@ -28,22 +77,20 @@ def plain_inner_product(x, x1):
 maxLevelsRemaining = 5
 appRotIndex_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 logBsSlots_list = []
-logN = 14
+logN = 16
 dnum = 3
 dcrtBits = 48 
 firstMod = 60
 levelBudget_list = []
 rescaleTech = "FLEXIBLEAUTO"  # "FLEXIBLEAUTO" # "FIXEDMANUAL"
-save_dir = "torch/fhe/data/"
 mode = "release"  # "debug" or "release"
 autoLoadAndSetConfig = True # note: currently only support True
 
-if not os.path.exists(save_dir):
-    raise ValueError(f"Directory {save_dir} does not exist!")
+DATA_DIR = os.environ["DATA_DIR"]
 
 cryptoContext, openfhe_context = (
     fhe.try_load_context(maxLevelsRemaining, appRotIndex_list, logBsSlots_list, logN, dnum, dcrtBits, firstMod,
-                         levelBudget_list, "UNIFORM_TERNARY", rescaleTech, save_dir=save_dir,
+                         levelBudget_list, "UNIFORM_TERNARY", rescaleTech, save_dir=DATA_DIR,
                          autoLoadAndSetConfig=True, mode=mode))
 
 
@@ -59,14 +106,29 @@ x1 = torch.tensor(x1, device="cuda")
 ptx = fhe.encode(x1, 1, 0, encode_slots, use_gpu_fft=True, cryptoContext=cryptoContext)
 cipher1 = openfhe_context.encrypt(x1, 1, openfhe_context.depth - 1, encode_slots, mode)
 
-# clear_result = openfhe_context.decrypt(cipher1)  
-# clear_result = clear_result.cpu().numpy().reshape(-1)
-# print("HE decryption result: ", clear_result[0])
-# inner_product
+torch.cpu.synchronize()
+torch.cuda.synchronize()
+start_time = time.time()
 cipher_inner_product = homo_inner_product(cipher,cipher1,cryptoContext)
+torch.cpu.synchronize()
+torch.cuda.synchronize()
+print("time: ", time.time() - start_time)
 print("homo_inner_product done!")
-
-clear_result = openfhe_context.decrypt(cipher_inner_product)  
+clear_result = openfhe_context.decrypt(cipher_inner_product)
 clear_result = clear_result.cpu().numpy().reshape(-1)
 print("HE decryption result: ", clear_result[0])
-# print("plain",plain_inner_product(x, x1))
+
+torch.cpu.synchronize()
+torch.cuda.synchronize()
+start_time = time.time()
+cipher_inner_product_hoisting = homo_inner_product_hoisting(cipher,cipher1,cryptoContext)
+torch.cpu.synchronize()
+torch.cuda.synchronize()
+print("time: ", time.time() - start_time)
+print("homo_inner_product done!")
+print("homo_inner_product_hoisting done!")
+clear_result = openfhe_context.decrypt(cipher_inner_product_hoisting)
+clear_result = clear_result.cpu().numpy().reshape(-1)
+print("HE decryption result: ", clear_result[0])
+
+print("plain",plain_inner_product(x, x1))
