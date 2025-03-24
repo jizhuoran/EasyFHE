8780bd4d60966d04cf6f416f8b2467fd84f03a79
yhh <931323942@qq.com>
2025-03-24 22:52:57 +0800

    refactor homo_inner_product_hoisting, add homo_inner_product_hybrid

--- examples/inner_product/innerproduct_example.py
+++ examples/inner_product/innerproduct_example.py
@@ -13,12 +13,11 @@ import torch
 import os
 import math
 
-
-def homo_inner_product(cipher_A, cipher_B, cryptoContext):
+@fhe.utils.profile_python_function
+def homo_inner_product(cipher_A, cipher_B, n, cryptoContext):
+    assert (n & (n - 1)) == 0, "n must be a power of two"
     cipher_product = fhe.homo_mul(cipher_A, cipher_B, cryptoContext)
     cipher_product = fhe.homo_rescale(cipher_product, 1, cryptoContext)
-    n = cipher_product.slots
-    assert (n & (n - 1)) == 0, "n must be a power of two"
     log_n = int(math.log2(n))
     shifts = [2**i for i in range(log_n)] 
     for shift in shifts:
@@ -27,12 +26,11 @@ def homo_inner_product(cipher_A, cipher_B, cryptoContext):
     
     return cipher_product
 
-
-def homo_inner_product_hoisting(cipher_A, cipher_B, cryptoContext):
+@fhe.utils.profile_python_function
+def homo_inner_product_hoisting(cipher_A, cipher_B, n, cryptoContext):
+    assert (n & (n - 1)) == 0, "n must be a power of two"
     cipher_product = fhe.homo_mul(cipher_A, cipher_B, cryptoContext)
     cipher_product = fhe.homo_rescale(cipher_product, 1, cryptoContext)
-    n = cipher_product.slots
-    assert (n & (n - 1)) == 0, "n must be a power of two"
     log_n = int(math.log2(n))
     shifts = [2 ** i for i in range(log_n)]
 
@@ -51,34 +49,165 @@ def homo_inner_product_hoisting(cipher_A, cipher_B, cryptoContext):
         # rotated = fhe.cipher_automorphism(rotated, cryptoContext.norm_rot_index(shift), cryptoContext, printInfo=False)
 
         # ver3
+        # rotated_modup = fhe.modup_to_ext(ax, cryptoContext)
+        # norm_index = cryptoContext.norm_rot_index(shift)
+        # rotated_innerp = fhe.mult_rot_key_and_sum_ext(rotated_modup, norm_index, cryptoContext)
+        #
+        # tmp_bxExt = fhe.homo_add(fhe.extract_cv(rotated_innerp,0, cryptoContext), bxExt, cryptoContext)
+        # tmp_ax    = fhe.moddown_from_ext(fhe.extract_cv(rotated_innerp,1, cryptoContext), cryptoContext)
+        #
+        # tmp_bxExt = fhe.cipher_automorphism(tmp_bxExt, norm_index, cryptoContext)
+        # tmp_ax  = fhe.cipher_automorphism(tmp_ax, norm_index, cryptoContext)
+        #
+        # bxExt = fhe.homo_add(tmp_bxExt, bxExt, cryptoContext)
+        # ax    = fhe.homo_add(tmp_ax, ax, cryptoContext)
+
+        # ver4
         rotated_modup = fhe.modup_to_ext(ax, cryptoContext)
-        norm_index = cryptoContext.norm_rot_index(shift)
-        rotated_innerp = fhe.mult_rot_key_and_sum_ext(rotated_modup, norm_index, cryptoContext)
-
-        tmp_bxExt = fhe.homo_add(fhe.extract_cv(rotated_innerp,0, cryptoContext), bxExt, cryptoContext)
-        tmp_ax    = fhe.moddown_from_ext(fhe.extract_cv(rotated_innerp,1, cryptoContext), cryptoContext)
-
-        tmp_bxExt = fhe.cipher_automorphism(tmp_bxExt, norm_index, cryptoContext)
-        tmp_ax  = fhe.cipher_automorphism(tmp_ax, norm_index, cryptoContext)
+        tmp = fhe.eval_fast_rotate(rotated_modup, bxExt, shift, True, False, cryptoContext)
+        tmp_bxExt=fhe.extract_cv(tmp,0, cryptoContext)
+        tmp_ax = fhe.moddown_from_ext(fhe.extract_cv(tmp,1, cryptoContext), cryptoContext)
 
         bxExt = fhe.homo_add(tmp_bxExt, bxExt, cryptoContext)
-        ax    = fhe.homo_add(tmp_ax, ax, cryptoContext)
+        ax = fhe.homo_add(tmp_ax, ax, cryptoContext)
 
     bx = fhe.moddown_from_ext(bxExt, cryptoContext)
     cipher_product = cipher_product.cipher_like([bx.cv[0], ax.cv[0]])
 
     return cipher_product
 
+@fhe.utils.profile_python_function
+def homo_inner_product_hybrid(cipher_A, cipher_B, n, cryptoContext):
+    assert (n & (n - 1)) == 0, "n must be a power of two"
+    assert n in [8, 32, 128, 512], "Currently only supports n in [8, 32, 128, 512]"
+    cipher_product = fhe.homo_mul(cipher_A, cipher_B, cryptoContext)
+    cipher_product = fhe.homo_rescale(cipher_product, 1, cryptoContext)
+
+
+    digits_ext = fhe.modup_to_ext(
+        fhe.extract_cv(cipher_product, 1, cryptoContext), cryptoContext
+    )
+
+    fast_rotation_ext=[]
+    fast_rotation_ext.append(fhe.key_switch_P_ext(cipher_product, cryptoContext))
+    for i in range(1, 8):
+        fast_rotation_ext.append(
+            fhe.eval_fast_rotate(
+                digits_ext, cipher_product, i, True, False, cryptoContext
+            )
+        )
+
+    # sum up
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0],fast_rotation_ext[1],cryptoContext)
+    fast_rotation_ext[2] = fhe.homo_add(fast_rotation_ext[2],fast_rotation_ext[3],cryptoContext)
+    fast_rotation_ext[4] = fhe.homo_add(fast_rotation_ext[4],fast_rotation_ext[5],cryptoContext)
+    fast_rotation_ext[6] = fhe.homo_add(fast_rotation_ext[6],fast_rotation_ext[7],cryptoContext)
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0],fast_rotation_ext[2],cryptoContext)
+    fast_rotation_ext[4] = fhe.homo_add(fast_rotation_ext[4],fast_rotation_ext[6],cryptoContext)
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0],fast_rotation_ext[4],cryptoContext)
+
+    cipher_product = fast_rotation_ext[0]
+
+    if (n<=8):
+        return fhe.moddown_from_ext(cipher_product, cryptoContext)
+
+    tmp_bxExt = fhe.extract_cv(cipher_product, 0, cryptoContext)
+    ax = fhe.moddown_from_ext(fhe.extract_cv(cipher_product, 1, cryptoContext), cryptoContext)
+
+    digits_ext = fhe.modup_to_ext(ax, cryptoContext)
+
+    fast_rotation_ext = []
+    start_idx = 8
+    for i in range(start_idx, start_idx*4, start_idx):
+        fast_rotation_ext.append(
+            fhe.eval_fast_rotate(
+                digits_ext, cipher_product, i, True, False, cryptoContext
+            )
+        )
+
+    # sum up
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0], fast_rotation_ext[1], cryptoContext)
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0], fast_rotation_ext[2], cryptoContext)
+
+    cipher_product = fast_rotation_ext[0]
+    tmp_bxExt = fhe.homo_add(fhe.extract_cv(cipher_product, 0, cryptoContext), tmp_bxExt, cryptoContext)
+    ax = fhe.homo_add(fhe.moddown_from_ext(fhe.extract_cv(cipher_product, 1, cryptoContext), cryptoContext),ax,cryptoContext)
+
+    if n<=32:
+        bx = fhe.moddown_from_ext(tmp_bxExt, cryptoContext)
+        cipher_product = cipher_product.cipher_like([bx.cv[0], ax.cv[0]])
+        cipher_product.is_ext = False
+        return cipher_product
+
+    digits_ext = fhe.modup_to_ext(ax, cryptoContext)
+
+    fast_rotation_ext = []
+    start_idx = 32
+    for i in range(start_idx, start_idx * 4, start_idx):
+        fast_rotation_ext.append(
+            fhe.eval_fast_rotate(
+                digits_ext, tmp_bxExt, i, True, False, cryptoContext
+            )
+        )
+
+    # sum up
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0], fast_rotation_ext[1], cryptoContext)
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0], fast_rotation_ext[2], cryptoContext)
+
+
+    tmp_bxExt = fhe.homo_add(fhe.extract_cv(fast_rotation_ext[0], 0, cryptoContext), tmp_bxExt, cryptoContext)
+    ax = fhe.homo_add(fhe.moddown_from_ext(fhe.extract_cv(fast_rotation_ext[0], 1, cryptoContext), cryptoContext),ax,cryptoContext)
+
+    if n<=128:
+        bx = fhe.moddown_from_ext(tmp_bxExt, cryptoContext)
+        cipher_product = cipher_product.cipher_like([bx.cv[0], ax.cv[0]])
+        return cipher_product
+
+
+    digits_ext = fhe.modup_to_ext(ax, cryptoContext)
+
+    fast_rotation_ext = []
+    start_idx = 128
+    for i in range(start_idx, start_idx * 4, start_idx):
+        fast_rotation_ext.append(
+            fhe.eval_fast_rotate(
+                digits_ext, tmp_bxExt, i, True, False, cryptoContext
+            )
+        )
+
+    # sum up
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0], fast_rotation_ext[1], cryptoContext)
+    fast_rotation_ext[0] = fhe.homo_add(fast_rotation_ext[0], fast_rotation_ext[2], cryptoContext)
+
+    tmp_bxExt = fhe.homo_add(fhe.extract_cv(fast_rotation_ext[0], 0, cryptoContext), tmp_bxExt, cryptoContext)
+    ax = fhe.homo_add(fhe.moddown_from_ext(fhe.extract_cv(fast_rotation_ext[0], 1, cryptoContext), cryptoContext),ax,cryptoContext)
+
+    if n<=512:
+        bx = fhe.moddown_from_ext(tmp_bxExt, cryptoContext)
+        cipher_product = cipher_product.cipher_like([bx.cv[0], ax.cv[0]])
+        return cipher_product
+
+
+
 def plain_inner_product(x, x1):
     return torch.dot(x, x1)
 
+encode_slots = (1 << 9)
+print("encode_slots:", encode_slots)
+maxLevelsRemaining = 15
+# appRotIndex_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+appRotIndex_list = [1, 2, 3, 4, 5, 6, 7,
+                    8, 16, 24, 32,
+                    64, 96, 128,
+                    256, 384, 512,
+                    1024
+                    ]
 
-maxLevelsRemaining = 5
-appRotIndex_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
 logBsSlots_list = []
 logN = 16
 dnum = 3
-dcrtBits = 48 
+dcrtBits = 58
 firstMod = 60
 levelBudget_list = []
 rescaleTech = "FLEXIBLEAUTO"  # "FLEXIBLEAUTO" # "FIXEDMANUAL"
@@ -93,44 +222,66 @@ cryptoContext, openfhe_context = (
                          levelBudget_list, "UNIFORM_TERNARY", rescaleTech, save_dir=DATA_DIR, config=config))
 
 
-values = [0.111111, 0.222222, 0.333333, 0.444444, 0.555555, 0.666666, 0.777777, 0.888888]
-encode_slots = (1 << 11)
+# values = [0.111111, 0.222222, 0.333333, 0.444444, 0.555555, 0.666666, 0.777777, 0.888888]
+# values = [0.111111, 0.222222, 0.333333, 0.444444, 0.555555, 0.666666, 0.777777, 0.888888]
+values = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+
 x = np.array([values[i % len(values)] for i in range(encode_slots)])
 x = torch.tensor(x, device="cuda")
 cipher = openfhe_context.encrypt(x, 1, openfhe_context.depth - 1, encode_slots)
 
-values1 = [0.888888, 0.888888, 0.888888, 0.888888, 0.888888, 0.888888, 0.888888, 0.888888]
+values1 = [0.111111, 0.111111, 0.111111, 0.111111, 0.111111, 0.111111, 0.111111, 0.111111]
 x1 = np.array([values1[i % len(values1)] for i in range(encode_slots)])
 x1 = torch.tensor(x1, device="cuda")
 ptx = fhe.encode(x1, 1, 0, encode_slots, False, cryptoContext)
 cipher1 = openfhe_context.encrypt(x1, 1, openfhe_context.depth - 1, encode_slots)
 
-cipher_inner_product = homo_inner_product(cipher,cipher1,cryptoContext)
-torch.cpu.synchronize()
-torch.cuda.synchronize()
-start_time = time.time()
-cipher_inner_product = homo_inner_product(cipher,cipher1,cryptoContext)
-torch.cpu.synchronize()
-torch.cuda.synchronize()
-print("time: ", time.time() - start_time)
-print("homo_inner_product done!")
-clear_result = openfhe_context.decrypt(cipher_inner_product)
-clear_result = clear_result.cpu().numpy().reshape(-1)
-print("HE decryption result: ", clear_result[0], "\n\n")
+for n in [8, 32, 128, 512]:
+    print("vector length for inner product operations", n, "\n")
+
+    cipher_inner_product = homo_inner_product(cipher,cipher1, n, cryptoContext)
+    torch.cpu.synchronize()
+    torch.cuda.synchronize()
+    start_time = time.time()
+    cipher_inner_product = homo_inner_product(cipher,cipher1, n, cryptoContext)
+    torch.cpu.synchronize()
+    torch.cuda.synchronize()
+    print("time: ", time.time() - start_time)
+    print("homo_inner_product done!")
+    clear_result = openfhe_context.decrypt(cipher_inner_product)
+    clear_result = clear_result.cpu().numpy().reshape(-1)
+    print("HE decryption result: ", clear_result[0], "\n\n")
 
 
-cipher_inner_product_hoisting = homo_inner_product_hoisting(cipher,cipher1,cryptoContext)
-torch.cpu.synchronize()
-torch.cuda.synchronize()
-start_time = time.time()
-cipher_inner_product_hoisting = homo_inner_product_hoisting(cipher,cipher1,cryptoContext)
-torch.cpu.synchronize()
-torch.cuda.synchronize()
-print("time: ", time.time() - start_time)
-print("homo_inner_product done!")
-print("homo_inner_product_hoisting done!")
-clear_result = openfhe_context.decrypt(cipher_inner_product_hoisting)
-clear_result = clear_result.cpu().numpy().reshape(-1)
-print("HE decryption result: ", clear_result[0])
+    cipher_inner_product_hoisting = homo_inner_product_hoisting(cipher,cipher1, n, cryptoContext)
+    torch.cpu.synchronize()
+    torch.cuda.synchronize()
+    start_time = time.time()
+    cipher_inner_product_hoisting = homo_inner_product_hoisting(cipher,cipher1, n, cryptoContext)
+    torch.cpu.synchronize()
+    torch.cuda.synchronize()
+    print("time: ", time.time() - start_time)
+    print("homo_inner_product done!")
+    print("homo_inner_product_hoisting done!")
+    clear_result = openfhe_context.decrypt(cipher_inner_product_hoisting)
+    clear_result = clear_result.cpu().numpy().reshape(-1)
+    print("HE decryption result: ", clear_result[0], "\n\n")
 
-print("plain",plain_inner_product(x, x1))
+
+    cipher_inner_product_hybrid = homo_inner_product_hybrid(cipher,cipher1, n, cryptoContext)
+    torch.cpu.synchronize()
+    torch.cuda.synchronize()
+    start_time = time.time()
+    cipher_inner_product_hybrid = homo_inner_product_hybrid(cipher,cipher1, n, cryptoContext)
+    torch.cpu.synchronize()
+    torch.cuda.synchronize()
+    print("time: ", time.time() - start_time)
+    print("homo_inner_product done!")
+    print("homo_inner_product_hybrid done!")
+    clear_result = openfhe_context.decrypt(cipher_inner_product_hybrid)
+    clear_result = clear_result.cpu().numpy().reshape(-1)
+    print("HE decryption result: ", clear_result[0], "\n\n")
+
+    print("plain",plain_inner_product(x, x1))
+
+    print("\n\n")
