2e2856c2e2fef0bb234b0a4f6408559d81ab67bd
yhh <931323942@qq.com>
2025-04-10 02:22:22 +0800

    [LSTM-RNN] not work, still lots of bugs

--- examples/RNN/src/rnn.py
+++ examples/RNN/src/rnn.py
@@ -1,37 +1,15 @@
-import time
-import torch.fhe as fhe
-from torch.fhe.bootstrapping import eval_bootstrap
-import numpy as np
+import os, sys
+sys.path.append("/".join(os.getcwd().split("/")[:-4]))
+sys.path.append("/".join(os.getcwd().split("/")[:-3]))
 import torch
-import torch.fhe.client.openfhe as openfhe
-from torch.fhe.ciphertext import Cipher
-import random
+import torch.fhe as fhe
+import numpy as np
 import math
-import os
+
+DATA_DIR = os.environ["DATA_DIR"]
 
 
-maxLevelsRemaining = 26
-appRotIndex_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
-logBsSlots_list = [8] # todo: should be 8 for mnist data
-logN = 16
-dnum = 3
-dcrtBits = 46
-firstMod = 50
-levelBudget_list = [[2, 2]]
-rescaleTech = "FIXEDAUTO"  # "FLEXIBLEAUTO" # "FIXEDMANUAL"
-save_dir = "/home/cys/PNP/GPU-FHE/examples/RNN/data"
-mode = "release"  # "debug" or "release"
-autoLoadAndSetConfig = True # note: currently only support True
-encode_slots = (1 << (logN -1))
-
-
-if not os.path.exists(save_dir):
-    raise ValueError(f"Directory {save_dir} does not exist!")
-
-cryptoContext, openfhe_context = (
-    fhe.try_load_context(maxLevelsRemaining, appRotIndex_list, logBsSlots_list, logN, dnum, dcrtBits, firstMod,
-                        levelBudget_list, "SPARSE_TERNARY", rescaleTech, save_dir=save_dir,
-                        autoLoadAndSetConfig=True, mode=mode))
+encode_slots = int(1 << (16 - 1)) #todo: to be removed
 
 rnn_ih = []
 rnn_hh = []
@@ -39,11 +17,11 @@ fc_weight = []
 fc_bias = None
 
 
-def rnn_layer(input, hidden, cryptoContext):
+def rnn_layer(input, hidden, cryptoContext, openfhe_context):
     # Evaluate tanh(rnn_ih * input + rnn_hh * hidden)
     # tanh() is approximated by x - (x^3) / 3 (Taylor Series, could be optimized)
     # Weights are always within (-1, 1), so an approximation in (-2, 2) is good enough
-    num_slots = (1 << (logN -1))
+    num_slots = (1 << (cryptoContext.logN -1))
     num_batch = num_slots / 128
     # Rotation are done based on batch size
     hidden_accum_ct = openfhe_context.encrypt(np.zeros(num_slots), 1, 0, encode_slots)
@@ -62,7 +40,7 @@ def rnn_layer(input, hidden, cryptoContext):
 
     # Apply tanh activation (approximation: （ax + bx^3）)
     x2 = fhe.homo_square(hidden_accum_ct, cryptoContext)
-    x2 = fhe.homo_rescale(x2, 1, cryptoContext)
+    x2 = fhe.homo_rescale(x2, 1, cryptoContext) # todo: ??
     x3 = fhe.homo_mul_scalar_double(hidden_accum_ct, -0.10484599)
     x2 = fhe.homo_rescale(x3, 1, cryptoContext)
     hidden_accum_ct = fhe.homo_mul_scalar_double(hidden_accum_ct, 0.86501289)
@@ -72,9 +50,9 @@ def rnn_layer(input, hidden, cryptoContext):
     hidden_accum_ct = fhe.homo_add(hidden_accum_ct, t_x3)
     return hidden_accum_ct
 
-def fc_layer(input, cryptoContext):
+def fc_layer(input, cryptoContext, openfhe_context):
     # Evaluate fc_weight * input + bias
-    num_slots = (1 << (logN -1))
+    num_slots = (1 << (cryptoContext.logN -1))
     num_batch = num_slots / 128
     output_accum_ct = openfhe_context.encrypt(np.zeros(num_slots), 1, 0, encode_slots)
     # Matrix-Vector Multiplication
@@ -93,35 +71,56 @@ def activation(t):
 def sigmoid(t):
     return 1 / (1 + math.exp(-t))
 
-def fhe_rnn(b_id, cryptoContext):
+def fhe_rnn(b_id):
     EMBEDDING_SIZE = 128
     STEP_NUM = 128
     sample_num = 256
 
-    # todo:初始化我提到外面去了
+    # todo: move inside a func
+    logN = 16
+    # encode_slots = int(1 << (logN - 1))
+    maxLevelsRemaining = 26
+    appRotIndex_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768] # todo: should be computed from input
+    logBsSlots_list = [int(math.log2(encode_slots))]
+    dnum = 1
+    dcrtBits = 46
+    firstMod = 50
+    levelBudget_list = [[4, 4]]
+    rescaleTech = "FIXEDAUTO"  # "FLEXIBLEAUTO" # "FIXEDMANUAL"
+    secretKeyDist = "SPARSE_TERNARY"
+
+    if not os.path.exists(DATA_DIR):
+        raise ValueError(f"Directory {DATA_DIR} does not exist!")
+
+    config = torch.fhe.config.Config(AUTO_LOAD_KEYS=True)
+    cryptoContext, openfhe_context = (
+        fhe.try_load_context(maxLevelsRemaining, appRotIndex_list, logBsSlots_list, logN, dnum, dcrtBits, firstMod,
+                             levelBudget_list, secretKeyDist, rescaleTech, save_dir=DATA_DIR,
+                             config=config))
 
     # File paths
-    embedding_file_name = f"/home/cys/PNP/GPU-FHE/examples/RNN/train/test_input/embedding_batch_{b_id}.bin"
-    ground_truth_file_name = f"/home/cys/PNP/GPU-FHE/examples/RNN/train/test_input/ground_truth_batch_{b_id}.bin"
+    embedding_file_name = f"../train/test_input/embedding_batch_{b_id}.bin"
+    ground_truth_file_name = f"../train/test_input/ground_truth_batch_{b_id}.bin"
 
     # Load weights and biases from binary files
-    rnn_ih_t = np.fromfile("/home/cys/PNP/GPU-FHE/examples/RNN/train/trained_rnn_ih.bin", dtype=np.float32).reshape(EMBEDDING_SIZE, STEP_NUM)
-    rnn_hh_t = np.fromfile("/home/cys/PNP/GPU-FHE/examples/RNN/train/trained_rnn_hh.bin", dtype=np.float32).reshape(STEP_NUM, STEP_NUM)
-    fc_weight_t = np.fromfile("/home/cys/PNP/GPU-FHE/examples/RNN/train/trained_fc_weight.bin", dtype=np.float32).reshape(2, STEP_NUM)
-    fc_bias_t = np.fromfile("/home/cys/PNP/GPU-FHE/examples/RNN/train/trained_fc_bias.bin", dtype=np.float32)
+    rnn_ih_t = np.fromfile("../train/trained_rnn_ih.bin", dtype=np.float32).reshape(EMBEDDING_SIZE, STEP_NUM)
+    rnn_hh_t = np.fromfile("../train/trained_rnn_hh.bin", dtype=np.float32).reshape(STEP_NUM, STEP_NUM)
+    fc_weight_t = np.fromfile("../train/trained_fc_weight.bin", dtype=np.float32).reshape(2, STEP_NUM)
+    fc_bias_t = np.fromfile("../train/trained_fc_bias.bin", dtype=np.float32)
+    fc_bias_t = fc_bias_t.astype(np.float64) # todo: change back to the previous 32 after merging the new encode?
 
     # Load embedding data and ground truth from binary files
-    embedding_in = np.fromfile(embedding_file_name, dtype=np.float32).reshape(sample_num, STEP_NUM, EMBEDDING_SIZE)
+    embedding_in = np.fromfile(embedding_file_name, dtype=np.float32)[:sample_num*STEP_NUM*EMBEDDING_SIZE].reshape(sample_num, STEP_NUM, EMBEDDING_SIZE)
     ground_truth = np.fromfile(ground_truth_file_name, dtype=np.float32)
 
     # Calculate batch size
-    batch_size = encode_slots // EMBEDDING_SIZE
+    batch_size = int(encode_slots // EMBEDDING_SIZE)
 
     # Pack the plaintext matrix in diagonal order
     rnn_ih_pt_vec = []
     rnn_hh_pt_vec = []
     fc_weight_pt_vec = []
-    fc_bias_pt_vec = []
+    fc_bias_pt_vec = [0.0]*batch_size*STEP_NUM
 
 # TODO 这里的原有的三层循环被我替换成了向量化操作，但我不确定这样是不是对的
     rnn_ih_t_2d = rnn_ih_t.reshape((EMBEDDING_SIZE, EMBEDDING_SIZE))
@@ -133,13 +132,15 @@ def fhe_rnn(b_id, cryptoContext):
         columns = np.arange(EMBEDDING_SIZE)
         elements = rnn_ih_t_2d[rows, columns]
         rnn_ih_pt = np.repeat(elements, batch_size)
-        rnn_ih.append(fhe.encode(rnn_ih_pt, 1, 0, encode_slots, True, cryptoContext))
+        rnn_ih_pt = torch.tensor(rnn_ih_pt, dtype=torch.float64).cuda() # todo: change back to the previous 32 after merging the new encode?
+        rnn_ih.append(fhe.encode(rnn_ih_pt, 1, 0, encode_slots, False, cryptoContext))
 
         rows1 = (np.arange(STEP_NUM) + i) % STEP_NUM
         columns1 = np.arange(STEP_NUM)
         elements = rnn_hh_t_2d[rows1, columns1]
         rnn_hh_pt = np.repeat(elements, batch_size)
-        rnn_hh.append(fhe.encode(rnn_hh_pt, 1, 0, encode_slots, True, cryptoContext))
+        rnn_hh_pt = torch.tensor(rnn_hh_pt, dtype=torch.float64).cuda() # todo: change back to the previous 32 after merging the new encode?
+        rnn_hh.append(fhe.encode(rnn_hh_pt, 1, 0, encode_slots, False, cryptoContext))
 
     fc_weight_t_2d = fc_weight_t.reshape(2, STEP_NUM)
     for i in range(2):
@@ -149,14 +150,17 @@ def fhe_rnn(b_id, cryptoContext):
         # Extract elements and repeat batch_size times
         elements = fc_weight_t_2d[rows, columns]
         fc_weight_pt = np.repeat(elements, batch_size)
-        fc_weight.append(fhe.encode(fc_weight_pt, 1, 0, encode_slots, True, cryptoContext))
+        fc_weight_pt = torch.tensor(fc_weight_pt, dtype=torch.float64).cuda() # todo: change back to the previous 32 after merging the new encode?
+        fc_weight.append(fhe.encode(fc_weight_pt, 1, 0, encode_slots, False, cryptoContext))
 
-    fc_bias_pt_vec = [fc_bias_t[j % 2] for j in range(STEP_NUM) for _ in range(batch_size)]
-    fc_bias = fhe.encode(fc_bias_pt_vec, 1, 0, encode_slots, True, cryptoContext)
+    for j in range(STEP_NUM):
+        for k in range(batch_size):
+            fc_bias_pt_vec[j * batch_size + k] = fc_bias_t[j % 2]
+    fc_bias = fhe.encode(fc_bias_pt_vec, 1, 0, encode_slots, False, cryptoContext)
     print("Finished building RNN weight plaintexts!")
 
     batched_hidden_ct = openfhe_context.encrypt(np.zeros(batch_size * STEP_NUM), 1, 0, encode_slots)
-    print(f"Before rnn, batched_hidden_ct's remaining levels: {maxLevelsRemaining - batched_hidden_ct.L }")
+    print(f"Before rnn, batched_hidden_ct's remaining levels: {cryptoContext.L - batched_hidden_ct.cur_limbs}")
 
     #todo  Perform key generation (if needed)
     batch_id = 0
@@ -178,7 +182,7 @@ def fhe_rnn(b_id, cryptoContext):
         print("Evaluating Bootstrapping!")
         batched_hidden_ct = fhe.homo_bootstrap(batched_hidden_ct, L0=cryptoContext.L, logBsSlots=8, cryptoContext=cryptoContext)
         
-    batched_hidden_ct = rnn_layer(batched_embedding_ct, batched_hidden_ct, cryptoContext)
+    batched_hidden_ct = rnn_layer(batched_embedding_ct, batched_hidden_ct, cryptoContext, openfhe_context)
 
     # Run reference RNN computation
     result_ref = np.zeros((128, batch_size))
@@ -206,7 +210,7 @@ def fhe_rnn(b_id, cryptoContext):
     batched_hidden_ref = result_ref.copy() 
 
     # 2. Evaluate FC layers
-    fhe_result_pt = openfhe_context.encrypt(fc_layer(batched_hidden_ct, cryptoContext), 1, 0, encode_slots)
+    fhe_result_pt = openfhe_context.encrypt(fc_layer(batched_hidden_ct, cryptoContext, openfhe_context), 1, 0, encode_slots)
     # Run reference FC computation
     result_ref = np.zeros((2*batch_size), dtype=np.float64)
     result_fhe = np.zeros((2*batch_size), dtype=np.float64)
@@ -223,7 +227,6 @@ def fhe_rnn(b_id, cryptoContext):
     num_ref_correct = 0
     num_fhe_correct = 0
 
-    # 遍历批次中的每个样本
     for k in range(batch_size):
         print(f"gt: {ground_truth[k]} ref out: [{result_ref[k]} , {result_ref[k + batch_size]}] "
             f"fhe out: [{result_fhe[k]} , {result_fhe[k + batch_size]}]")
@@ -245,4 +248,5 @@ def fhe_rnn(b_id, cryptoContext):
     print(f"fhe accuracy:\t{num_fhe_correct}/\t{num_inf}\t{100.0 * num_fhe_correct / num_inf:.2f}%")
 
 if __name__ == "__main__":
-    fhe_rnn()
+    for b_id in range(1):
+        fhe_rnn(b_id)
