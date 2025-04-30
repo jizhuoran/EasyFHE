12d54d88e5a1574b36bb371dc10b6904ed013727
yhp <yhp@email.com>
2025-04-30 18:19:12 +0800

    break moddown from ks

--- aten/src/ATen/native/fhe/cpu/KeySwitch.h
+++ aten/src/ATen/native/fhe/cpu/KeySwitch.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <cstdint>
 #include <ATen/core/Tensor.h>
+#include <cstdint>
 
 namespace at::native {
 void iNTT_impl(
@@ -25,18 +25,18 @@ void NTT_impl(
     const Tensor& param_primes,
     const Tensor& param_power_of_roots);
 
-    void NTT_except_some_range_impl(
-        uint64_t* op_ptr,
-        int64_t start_prime_idx,
-        int64_t batch,
-        int64_t param_degree,
-        int64_t excluded_range_start,
-        int64_t excluded_range_size,
-        int64_t curr_limbs,
-        int64_t level,
-        const Tensor& param_power_of_roots_shoup,
-        const Tensor& param_primes,
-        const Tensor& param_power_of_roots);
+void NTT_except_some_range_impl(
+    uint64_t* op_ptr,
+    int64_t start_prime_idx,
+    int64_t batch,
+    int64_t param_degree,
+    int64_t excluded_range_start,
+    int64_t excluded_range_size,
+    int64_t curr_limbs,
+    int64_t level,
+    const Tensor& param_power_of_roots_shoup,
+    const Tensor& param_primes,
+    const Tensor& param_power_of_roots);
 
 void switch_modulus(
     uint64_t* ptr,
@@ -85,3 +85,13 @@ void SubInplace(
     const Tensor& primes);
 
 } // namespace at::native
+
+namespace fhe {
+__uint128_t accumulate_in_modup(
+    const uint64_t* ptr,
+    const int degree,
+    const uint64_t* hat_mod_end,
+    const int start_length, // sizeP
+    const int degree_idx,
+    const int hat_mod_end_idx);
+}
