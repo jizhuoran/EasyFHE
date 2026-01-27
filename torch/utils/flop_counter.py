pytorch/pytorch

commit fedfc4db331466a5efe06f0a916d1042577165f0

[BE] Don't print 12 `triton not found` on import (#172614)
If one attempts to import `torch.utils.flop_count` on MacOS (or any other CPU system), they'll be greeted with the following repeated warning
```
% python -c "import torch.utils.flop_counter"
W0115 18:52:55.246000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.246000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.246000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
W0115 18:52:55.247000 34027 torch/utils/flop_counter.py:45] triton not found; flop counting will not work for triton kernels
```
which was introduced by https://github.com/pytorch/pytorch/pull/169876

torch/utils/flop_counter.py
@@ -22,6 +22,14 @@ _P = ParamSpec("_P")
 log = logging.getLogger(__name__)
 
 
+try:
+    from triton.runtime.jit import JITFunction as _JITFunction
+except ImportError:
+    if any(x is not None for x in [torch.version.cuda, torch.version.hip, torch.version.xpu]):
+        log.warning("triton not found; flop counting will not work for triton kernels")
+    _JITFunction = NoneType
+
+
 aten = torch.ops.aten
 
 def get_shape(i):
@@ -39,18 +47,13 @@ def shape_wrapper(f):
     return nf
 
 def register_flop_formula(targets, get_raw=False) -> Callable[[Callable[_P, _T]], Callable[_P, _T]]:
-    try:
-        from triton.runtime.jit import JITFunction
-    except ImportError:
-        log.warning("triton not found; flop counting will not work for triton kernels")
-        JITFunction = NoneType
 
     def register_fun(flop_formula: Callable[_P, _T]) -> Callable[_P, _T]:
         if not get_raw:
             flop_formula = shape_wrapper(flop_formula)
 
         def register(target) -> None:
-            if not (isinstance(target, (torch._ops.OpOverloadPacket, JITFunction))):
+            if not (isinstance(target, (torch._ops.OpOverloadPacket, _JITFunction))):
                 raise ValueError(
                     f"register_flop_formula(targets): expected each target to be "
                     f"OpOverloadPacket (i.e. torch.ops.mylib.foo), or JitFunction"

jizhuoran/EasyFHE

commit 2176b6ff138df6d47220e8fc7a0db7a6ccc30da5

add hasattr check when print 'triton not found' warning (#173397)
Summary:
D90903684 is breaking our torchstream tests and this should fix it.
see https://www.internalfb.com/intern/test/562950222982678.

Test Plan: buck test -j 18 fbsource//arvr/mode/mac-arm/opt fbcode//frl/ctrl/src2-main/platform/torchstream_ctrl/torchstream_ctrl/prod/tests:test_modules-arvr -- --exact 'fbcode//frl/ctrl/src2-main/platform/torchstream_ctrl/torchstream_ctrl/prod/tests:test_modules-arvr - test_ptq_compatibility (torchstream_ctrl.prod.tests.test_modules.Test_TdsBlock)' --stress-runs 20 --record-results

Reviewed By: ogbiggles

Differential Revision: D91038779

Regression caused by https://github.com/pytorch/pytorch/pull/172614
Pull Request resolved: https://github.com/pytorch/pytorch/pull/173397
Approved by: https://github.com/malfet

torch/utils/flop_counter.py
@@ -25,7 +25,7 @@ log = logging.getLogger(__name__)
 try:
     from triton.runtime.jit import JITFunction as _JITFunction
 except ImportError:
-    if any(x is not None for x in [torch.version.cuda, torch.version.hip, torch.version.xpu]):
+    if any(getattr(torch.version, attr, None) is not None for attr in ["cuda", "hip", "xpu"]):
         log.warning("triton not found; flop counting will not work for triton kernels")
     _JITFunction = NoneType
 